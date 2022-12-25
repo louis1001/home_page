b193c9651029af51d05501046cbfb2db8ea89799
louis1001 <louis1001dev@gmail.com>
2022-12-25 11:10:39 -0600

    Add link to mockup page

--- pages/demo/index.vue
+++ pages/demo/index.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
     <h1>Design Demos</h1>
+    <section>
+      <p>
+        <a href="https://mockups.louis1001.dev"><strong>Design Mockups</strong></a> - Where I try to mimic another site's design.
+      </p>
+    </section>
+    <pre>Placeholder index. To be designed later</pre>
   </div>
 </template>
+
+<style>
+  a {
+    color: inherit;
+  }
+</style>
