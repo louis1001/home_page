f2056dce6d8c1d2e54a6721194c3d2877657beb7
louis1001 <louis1001dev@gmail.com>
2024-08-21 20:32:46 -0600

    Improve loading UI

--- src/routes/(internal)/(section)/interests/photography/+page.svelte
+++ src/routes/(internal)/(section)/interests/photography/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-// @ts-nocheck
-import BiggerPicture from "bigger-picture";
+import BiggerPicture, { type BiggerPictureInstance } from "bigger-picture";
 import "bigger-picture/scss";
 
 let previewUrls = $state([] as any[]);
+let isLoading = $state(true)
 
 const imageModules = import.meta.glob(
   '$lib/assets/photos/*.{jpeg,jpg}',
@@ -14,27 +14,30 @@ const imageModules = import.meta.glob(
   }
 )
 
-let bp
+let bp: BiggerPictureInstance
 
-$effect(async () => {
-  let result = []
-  for (let entry in imageModules) {
-    let module = imageModules[entry]
+$effect(() => {
+  (async function() {
+    let result = []
+    for (let entry in imageModules) {
+      let module = imageModules[entry]
 
-    let imageModule = await module()
+      let imageModule = await module()
 
-    result.push({
-      // @ts-ignore
-      fullImage: imageModule.default.img,
-      // @ts-ignore
-      enhanced: imageModule.default
-    })
-  }
-  previewUrls = result
-  
-  bp = BiggerPicture({
-    target: document.body
-  });
+      result.push({
+        // @ts-ignore
+        fullImage: imageModule.default.img,
+        // @ts-ignore
+        enhanced: imageModule.default
+      })
+    }
+    previewUrls = result
+    isLoading = false
+    
+    bp = BiggerPicture({
+      target: document.body
+    });
+  })()
 })
 
 function openGallery(e: Event) {
@@ -44,7 +47,7 @@ function openGallery(e: Event) {
 
   bp.open({
     items: imageLinks,
-    el: e.currentTarget
+    el: e.currentTarget ?? undefined
   })
 }
 </script>
@@ -59,6 +62,16 @@ function openGallery(e: Event) {
 </p>
 
 <p>Now that I have it, here are my experiments.</p>
+
+{#if isLoading}
+<div class="loading-spinner">
+  <div style="--i: 0"></div>
+  <div style="--i: 1"></div>
+  <div style="--i: 2"></div>
+  <div style="--i: 3"></div>
+  <div style="--i: 4"></div>
+</div>
+{/if}
 
 <section id="photos">
   {#each previewUrls as obj}
@@ -105,6 +118,30 @@ function openGallery(e: Event) {
   &:active
     transform: scale(0.9)
 
+.image-link
+  animation: shimmer 1s ease-in-out infinite forwards
+  background: var(--background-col)
+
+.loading-spinner
+  position: relative
+  height: 3rem
+  margin: 1rem auto
+
+  div
+    position: absolute
+    width: 3rem
+    height: 3rem
+    left: 50%
+    transform: translateX(-50%)
+    opacity: .5
+    display: inline-block
+    border-radius: 100%
+    border: .5rem solid var(--main-col)
+    border-bottom: .5rem solid #fff0
+    --duration: 2s
+    --delay: calc(-1 * var(--i) * var(--duration) * 0.22)
+    animation: border-animation var(--duration) var(--delay) linear infinite forwards
+
 @media screen and (max-width: 1250px)
   #photos
     grid-template-columns: repeat(3, 1fr)
@@ -117,5 +154,31 @@ function openGallery(e: Event) {
 @media screen and (max-width: 580px)
   #photos
     grid-template-columns: repeat(1, 1fr)
+
+@keyframes shimmer
+  15%
+    background: var(--background-col)
+
+  50%
+    background: #222
+
+  85%
+    background: var(--background-col)
+
+@keyframes border-animation
+  0%
+    transform: scale(1) rotate(0deg)
+  
+  25%
+    transform: scale(.9) rotate(90deg)
+
+  50%
+    transform: scale(1) rotate(180deg)
+
+  75%
+    transform: scale(.8) rotate(270deg)
+  
+  100%
+    transform: rotate(360deg)
 
 </style>
